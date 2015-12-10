c4abc0bc6e8148202f1b8cbcf65d54fcc1aecf38
kievkao <kievkao@gmail.com>
2015-12-10 16:15:52 +0200

    Scroll with UIImageView

---
+++ KAOElasticHeaderExample/ComplexVC.swift
@@ -0,0 +1,53 @@
+//
+//  ComplexVC.swift
+//  KAOElasticHeaderExample
+//
+//  Created by Andrii Kravchenko on 12/10/15.
+//  Copyright © 2015 Andrii Kravchenko. All rights reserved.
+//
+
+import UIKit
+
+class ComplexVC: UITableViewController {
+    
+    private let kTableViewHeaderHeight: CGFloat = 200.0
+    private var headerView: UIView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.setupTableView()
+    }
+    
+    func setupTableView() {
+        self.headerView = self.tableView.tableHeaderView
+        self.tableView.tableHeaderView = nil
+        self.tableView.addSubview(self.headerView)
+        
+        self.tableView.contentInset = UIEdgeInsets(top: kTableViewHeaderHeight, left: 0, bottom: 0, right: 0)
+        self.tableView.contentOffset = CGPoint(x: 0, y: -kTableViewHeaderHeight)
+    }
+    
+    override func viewWillTransitionToSize(size: CGSize, withTransitionCoordinator coordinator: UIViewControllerTransitionCoordinator) {
+        super.viewWillTransitionToSize(size, withTransitionCoordinator: coordinator)
+        self.updateHeaderView(size.width)
+    }
+    
+    func updateHeaderView(parentWidth: CGFloat) {
+        
+        var headerRect = CGRect(x: 0, y: -kTableViewHeaderHeight, width: parentWidth, height: kTableViewHeaderHeight)
+        
+        if self.tableView.contentOffset.y < kTableViewHeaderHeight {
+            headerRect.origin.y = self.tableView.contentOffset.y
+            headerRect.size.height = -self.tableView.contentOffset.y
+        }
+        
+        self.headerView.frame = headerRect
+    }
+    
+    override func scrollViewDidScroll(scrollView: UIScrollView) {
+        self.updateHeaderView(self.tableView.bounds.width)
+    }
+
+
+}
